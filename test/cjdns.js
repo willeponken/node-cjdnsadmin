6e9df519066034f9f64a9a6c73845dbbc21e412f
willeponken <willeponken@gmail.com>
2014-12-21 14:52:56 +0100

    The Admin Functions

--- test/cjdns.js
+++ test/cjdns.js
@@ -11,8 +11,51 @@ describe('cjdns', function() {
 
       cjdns = new CJDNS(process.env['HOME'] + '/.cjdnsadmin');
 
-      //lalalala
       done();
+    });
+
+    describe('.ping(callback)', function() {
+      it('should ping admin backend and return "pong"', function(done) {
+
+        cjdns.ping(function(err, msg) {
+          assert.ifError(err); 
+          assert.equal(msg, 'pong');
+          
+          done();
+        });
+      });
+    });
+    
+    describe('.Admin_asyncEnabled(callback)', function() {
+      it('should return callback with info if async communication is enabled or not', function(done) {
+
+        cjdns.Admin_asyncEnabled(function(err, result) {
+          assert.ifError(err); 
+
+          if (!result === (false && true)) {
+            throw 'Wrong result ' + result;
+          }
+
+          done();
+        });
+      });
+    });
+    
+    describe('.Admin_availableFunctions(callback)', function() {
+      it('should return callback with array containing available functions', function(done) {
+
+        cjdns.Admin_availableFunctions(function(err, functions) {
+          assert.ifError(err);
+          assert.equal(typeof functions, 'object');
+
+          for (var func in functions) {
+            assert.equal(typeof functions[func].name, 'string');
+            assert.equal(typeof functions[func].params, 'object');
+          }
+
+          done();
+        });
+      });
     });
   });
 });
