0d791d195e55db37ec828e4483d93046c9c9e7b2
willeponken <willeponken@gmail.com>
2014-12-26 02:01:53 +0100

    Forked some code from cjdnsadmin.js in contrib. Now automatically imports all the methods available. Added some testing for them, will add more later tomorrow/today. Need to clean up and optimize some parts too

--- test/cjdns.js
+++ test/cjdns.js
@@ -1,8 +1,9 @@
 var CJDNS = require('../lib/cjdns.js'),
+    util = require('util'),
     fs = require('fs'),
     assert = require('assert');
 
-describe('cjdns', function() {
+describe('cjdnsadmin', function() {
   describe('CJDNS(config)', function() {
 
     var cjdns;
@@ -13,19 +14,86 @@ describe('cjdns', function() {
 
       done();
     });
+    
+    describe('Abstractions', function() {
+      describe('.availableFunctions(callback)', function() {
+        it('should return callback with array containing available functions', function(done) {
 
-    describe('.ping(callback)', function() {
-      it('should ping admin backend and return "pong"', function(done) {
+          cjdns.availableFunctions(function(err, functions) {
+            assert.ifError(err);
+            assert.equal(typeof functions, 'object');
 
-        cjdns.ping(function(err, msg) {
-          assert.ifError(err); 
-          assert.equal(msg, 'pong');
-          
-          done();
+            for (var func in functions) {
+              assert.equal(typeof functions[func].name, 'string');
+              assert.equal(typeof functions[func].params, 'object');
+            }
+
+            done();
+          });
         });
       });
     });
+
+    describe('Native', function() {
+      describe('.ping(callback)', function() {
+        it('should ping admin backend and return {"q": "pong"}', function(done) {
+
+          cjdns.ping(function(err, msg) {
+            assert.ifError(err); 
+            assert.equal(msg.q, 'pong');
+            
+            done();
+          });
+        });
+      });
+      
+      describe('.Admin_asyncEnabled(callback)', function() {
+        it('should return callback with info if async communication is enabled or not', function(done) {
+
+          cjdns.Admin_asyncEnabled(function(err, msg) {
+            assert.ifError(err); 
+
+            if (msg.asyncEnabled === 0 || msg.asyncEnabled === 1) {
+              done();
+            } else {
+              throw 'Wrong result ' + msg.asyncEnabled;
+            }
+
+          });
+        });
+      });
     
+      describe('.Admin_availableFunctions(callback)', function() {
+        it('should return callback with array containing available functions', function(done) {
+
+          cjdns.Admin_availableFunctions(0, function(err, functions) {
+            functions = functions.availableFunctions;
+            assert.ifError(err);
+            assert.equal(typeof functions, 'object');
+
+            for (var func in functions) {
+              assert.equal(typeof functions[func], 'object');
+            }
+
+            done();
+          });
+        });
+      });
+      
+      describe('.NodeStore_dumpTable(page, callback)', function() {
+        it('should take page and return callback with NodeStore table as an object', function(done) {
+
+            cjdns.NodeStore_dumpTable(0, function (err, table) {
+              assert.ifError(err);
+              assert.equal(typeof table, 'object');
+              
+              done()
+            });
+        });
+      });
+    });
+
+    /* 
     describe('.Admin_asyncEnabled(callback)', function() {
       it('should return callback with info if async communication is enabled or not', function(done) {
 
@@ -70,6 +138,6 @@ describe('cjdns', function() {
         });
 
       });
-    });
+    });*/
   });
 });
